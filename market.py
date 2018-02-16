726b4cf05bc68becc5a36b7d1288d1eeb3fc3272
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2018-02-16 09:33:25 +0100

    Solved?

--- market.py
+++ market.py
@@ -11,5 +11,23 @@ stockmkt = page_soup.findAll("tbody")
 mkt = stockmkt[0]
 rowmkt = mkt.findAll("tr")
 
+field_names = page_soup.findAll("thead")[0].findAll("tr")[0].findAll("th")
+
+field_name_rows = []
+values = []
+
+# get header fields
+for row in field_names:
+    field_name_rows.append(row.text)
+
+# loop through body
 for row in rowmkt:
-    print(row.text)
+    row_object = {}
+    # loop through fields
+    field_columns = row.findAll("td")
+    for idx, each_column in enumerate(field_columns):
+        row_object[field_name_rows[idx]] = each_column.text
+    values.append(row_object)
+
+# Values is now an array of everything
+print(values)
